318a3ecf7f1c9c7e8e6dbb525119b731d5332193
Bappi Kabir <kabirbapson@gmail.com>
2022-12-20 21:22:27 +0100

    first init

--- src/features/users/UserList.tsx
+++ src/features/users/UserList.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 
 const UserList = () => {
-  const users = [];
-  const renderUsers = () => {
-    return <div>Users Here</div>;
-  };
+  const users = [
+    { id: 1, name: "Musa Zazzau", email: "musa@email.com" },
+    { id: 2, name: "Kafi Zabuwa", email: "zabuwa@email.com" },
+  ];
+  const renderUsers = () =>
+    users.map((user) => (
+      <div
+        className="bg-gray-300 p-5 flex items-center justify-between"
+        key={user.id}
+      >
+        <div>
+          <h3 className="font-bold text-lg text-gray-700 ">{user.name} </h3>
+          <span className="font-normal text-gray-700 "> {user.email} </span>
+        </div>
+        <div className="" >
+        
+        </div>
+      </div>
+    ));
   return (
     <div className="grid gap-5 md:grid-cols-2 ">
       {users.length ? (
